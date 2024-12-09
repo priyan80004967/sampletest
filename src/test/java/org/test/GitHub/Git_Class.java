13c0f2f4f71fa45d19668f747d8ceb3beec2f4af
priyan80004967 <dharsanemail@gmail.com>
2024-12-09 23:54:46 +0530

    Initial Codes Git

---
+++ src/test/java/org/test/GitHub/Git_Class.java
@@ -0,0 +1,22 @@
+package org.test.GitHub;
+
+public class Git_Class {	
+
+private void method1() {
+	
+System.out.println("push");
+}	
+
+private void method2() {
+	System.out.println("Pull");
+
+}	
+
+private void method3() {
+	System.out.println("Commit");
+
+}	
+
+	
+	
+}
