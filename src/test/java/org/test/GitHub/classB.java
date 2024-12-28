f8e68f900f6a8a6dc61f60c7e8f24f1f557cf021
priyan80004967 <dharsanemail@gmail.com>
2024-12-28 12:21:26 +0530

    Class B push to branch

---
+++ src/test/java/org/test/GitHub/classB.java
@@ -0,0 +1,21 @@
+package org.test.GitHub;
+
+public class classB {
+
+	
+	private void Chennai() {
+		System.out.println("chennai");
+
+	}
+	
+	private void madurai() {
+		System.out.println("Madurai");
+
+	}
+	
+	private void trichy() {
+		System.out.println("Trichy");
+
+	}
+	
+}
